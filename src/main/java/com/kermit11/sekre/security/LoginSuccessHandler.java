f6294097f1bd46cb0ec89f2a985eb42348fbd5cc
kermit11 <kermit11@gmail.com>
2020-09-16 22:03:51 +0300

    FIX: Redirect after auth to main page

--- src/main/java/com/kermit11/sekre/security/LoginSuccessHandler.java
+++ src/main/java/com/kermit11/sekre/security/LoginSuccessHandler.java
@@ -2,6 +2,7 @@ package com.kermit11.sekre.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +19,15 @@ public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
      */
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException
     {
+        //We can't access super.requestCache, but it's the same one stored on the session
+        DefaultSavedRequest savedRequest = (DefaultSavedRequest) request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST");
+        //If user was redirected to login page while attempting to submit a form, resume the form submission
+        if (savedRequest!=null)
+        {
+            super.onAuthenticationSuccess(request, response, authentication);
+            return;
+        }
+        //Otherwise (=user clicked the "login" link directly), we still want to redirect to the original page (we stored it on the session), not to the default redirect target
         HttpSession session = request.getSession();
         String redirectUrl = (String) session.getAttribute("url_prior_login");
         if (redirectUrl != null)
